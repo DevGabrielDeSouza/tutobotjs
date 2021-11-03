c85a00db7768e121a7ba130bbe859b3cb8258d42
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-11-03 10:24:17 -0300

    Server couting and ping delay

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 require("dotenv").config();
+
+const config = require("./config.json"); 
+
 const { Client, Intents } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-    ]
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+	]
 });
 
-client.once("ready", () => {
-    console.log("TutoBot is ready!");
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`Bot iniciado com ${client.users.cache.size}, em ${client.channels.cache.size} canais, em ${client.guilds.cache.size} servidores!`);
+client.on("ready", () => {
+	console.log("TutoBot is ready!");
+	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Bot iniciado com ${client.users.cache.size}, em ${client.channels.cache.size} canais, em ${client.guilds.cache.size} servidores!`);
+	client.user.setActivity(`Estou em ${client.guilds.cache.size} servidores.`);
 });
 
-client.once("guildCreate", guild => {
-    console.log(`Bot entrou em um novo servidor!`);
-    console.log(`Nome: ${guild.name} (id: ${guild.id}). Populacão ${guild.memberCount} membros.`);
+client.on("guildCreate", guild => {
+	console.log(`Bot entrou em um novo servidor!`);
+	console.log(`Nome: ${guild.name} (id: ${guild.id}). Populacão ${guild.memberCount} membros.`);
+	client.user.setActivity(`Estou em ${client.guilds.cache.size} servidores.`);
 });
 
-client.once("guildDelete", guild => {
-    console.log(`O bot foi removido do servidor ${guild.name} (id: ${guild.id}).`);
+client.on("guildDelete", guild => {
+	console.log(`O bot foi removido do servidor ${guild.name} (id: ${guild.id}).`);
+	client.user.setActivity(`Estou em ${client.guilds.cache.size} servidores.`);
 });
 
-client.once("message", async message => {
-    if (message.author.bot) return;
-    if (message.channel.type == "dm") return;
-    if (message.content.startsWith("!ping")) {
-        message.channel.send("Pong!");
-    }
+client.on("messageCreate", async message => {
+	if (message.author.bot) return;
+	if (message.channel.type == "dm") return;
+	
+	if(!message.content.startsWith(config.prefix)) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const comando = args.shift().toLowerCase();
+	
+	// coamdno ping
+	if(comando === "ping") {
+		let m = await message.reply(`Pong! Calculando a latência...`);
+		m.edit(`Pong!\nA Latência é ${m.createdTimestamp - message.createdTimestamp}ms!\nA Latencia da API é ${Math.round(client.ws.ping)}ms!`);
+	}
 });
 
 client.login(process.env.TOKEN);
